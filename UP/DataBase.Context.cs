050285551ebd5a6d4f26634c31c1b485d9317890
БарашенковИТ <@208-07>
2023-03-10 15:31:39 +0300

    хз что это такое

--- UP/DataBase.Context.cs
+++ UP/DataBase.Context.cs
@@ -15,7 +15,6 @@ namespace UP
     
     public partial class Entities1 : DbContext
     {
-        private static Entities1 _instance;
         public Entities1()
             : base("name=Entities1")
         {
@@ -25,12 +24,6 @@ namespace UP
         {
             throw new UnintentionalCodeFirstException();
         }
-
-        public static Entities1 GetContex()
-        {
-            if (_instance == null) _instance = new Entities1();
-            return _instance;
-        }
     
         public virtual DbSet<Dolgnosti> Dolgnosti { get; set; }
         public virtual DbSet<Results> Results { get; set; }
@@ -38,5 +31,6 @@ namespace UP
         public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
         public virtual DbSet<Users> Users { get; set; }
         public virtual DbSet<Workers> Workers { get; set; }
+        public virtual DbSet<History> History { get; set; }
     }
 }
